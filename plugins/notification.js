bb3e2878905988e11b5fdcaec4934474a906ac8f
indahwii <widowati.indah07@gmail.com>
2020-08-26 10:14:58 +0700

    update link notif

--- plugins/notification.js
+++ plugins/notification.js
@@ -12,14 +12,32 @@ export default async function ({$axios}) {
         })
       }, 10000);
     });
-    self.addEventListener('notificationclick', function (event) {
-      var url = 'https://m.yoyaku.id/queue';
-      
+    self.onnotificationclick = function(event) {
+      console.log('On notification click: ', event.notification.tag);
       event.notification.close();
+    
+      // This looks to see if the current is already open and
+      // focuses if it is
+      event.waitUntil(clients.matchAll({
+        type: "window"
+      }).then(function(clientList) {
+        for (var i = 0; i < clientList.length; i++) {
+          var client = clientList[i];
+          if (client.url == 'https://m.yoyaku.id/queue' && 'focus' in client)
+            return client.focus();
+        }
+        if (clients.openWindow)
+          return clients.openWindow('https://m.yoyaku.id/queue');
+      }));
+    }
+  //   self.addEventListener('notificationclick', function (event) {
+  //     var url = 'https://m.yoyaku.id/queue';
+      
+  //     event.notification.close();
 
-      event.waitUntil(
-        clients.openWindow(url)
-      );
-      // clients.openWindow("https://m.yoyaku.id/queue");
-  });
+  //     event.waitUntil(
+  //       clients.openWindow(url)
+  //     );
+  //     // clients.openWindow("https://m.yoyaku.id/queue");
+  // });
 }
